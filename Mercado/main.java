86bece9411c88d5681dc250e341986ad1016ca04
aron-dan <82916233+aron-dan@users.noreply.github.com>
2022-09-30 02:15:03 -0300

    V1

---
+++ Mercado/main.java
@@ -0,0 +1,8 @@
+package Mercado;
+
+public class main {
+  public static void main(String[] args) {
+    
+  }
+  
+}
--- Mercado/src/teste.java
+++
@@ -1,9 +0,0 @@
-import javax.print.attribute.standard.RequestingUserName;
-
-public class teste {
-  public static void main(String[] args)
-   {
-    System.out.println("Helhow World!");
-  }
-  Return main;
-}
